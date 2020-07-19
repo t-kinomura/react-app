2e977b6133b54021a80c7144a5ff4806c5ee544e
Testu <tetsu99soccer@gmail.com>
2020-07-19 18:47:08 +0900

    Add: add react

---
+++ crud_front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MainContainer />
+      </div>
+    )
+  }
+}
+
+export default App;
